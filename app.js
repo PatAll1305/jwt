4905198a0f679cf5978f42de1e5e53309fdcf3a0
Patrick Allen <patrickallen1305@gmail.com>
2024-06-11 09:51:23 -0500

    completed work

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 require('dotenv').config();
 
 // Import package
-
+const jwt = require('jsonwebtoken');
 // Your code here 
 
 // Define variables - DO NOT MODIFY
@@ -11,7 +11,7 @@ require('dotenv').config();
 let token; // DO NOT MODIFY! Re-assign the token variable below.
 
 // Your code here 
-
+token = jwt.sign({ email: 'thisIsMyEmail@gmail.com' }, process.env.SECRET_KEY, { expiresIn: '2h' });
 // See the JWT in the console - DO NOT MODIFY
 console.log('JWT:', token);
 
@@ -20,7 +20,7 @@ console.log('JWT:', token);
 let payload; // DO NOT MODIFY! Re-assign the payload variable below.
 
 // Your code here 
-
+payload = jwt.decode(token)
 // See the decoded payload in the console - DO NOT MODIFY
 console.log('Payload:', payload);
 
@@ -29,7 +29,7 @@ console.log('Payload:', payload);
 let verifiedPayload; // DO NOT MODIFY! Re-assign the verifiedPayload variable below.
 
 // Your code here 
-
+verifiedPayload = jwt.verify(token, process.env.SECRET_KEY);
 // See the verified payload in the console - DO NOT MODIFY
 console.log('Verified Payload:', verifiedPayload);
 
